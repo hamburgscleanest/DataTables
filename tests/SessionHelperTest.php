4caae01b113f5fb0551e4a73556c2458e386084b
Chroma <chroma91@gmail.com>
2017-04-09 22:43:10 +0200

    SessionHelper: Tests

---
+++ tests/SessionHelperTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace hamburgscleanest\DataTables\Tests;
+
+use hamburgscleanest\DataTables\Helpers\SessionHelper;
+use Illuminate\Http\Request;
+use Mockery;
+
+/**
+ * Class SessionHelperTest
+ * @package hamburgscleanest\DataTables\Tests
+ */
+class SessionHelperTest extends TestCase {
+
+    private $_request;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->_request = Mockery::mock(Request::class);
+
+        $this->_request
+            ->shouldReceive('session')->andReturn(session())
+            ->shouldReceive('url')->andReturn('localhost');
+    }
+
+    /**
+     * @test
+     */
+    public function unset_state_returns_null()
+    {
+        $this->assertNull(SessionHelper::getState($this->_request, 'test'));
+    }
+
+    /**
+     * @test
+     */
+    public function saving_data_in_the_session_works()
+    {
+        SessionHelper::saveState($this->_request, 'test', true);
+        $this->assertTrue(SessionHelper::getState($this->_request, 'test'));
+    }
+
+    /**
+     * @test
+     */
+    public function removing_data_from_the_session_works()
+    {
+        SessionHelper::saveState($this->_request, 'test', true);
+        SessionHelper::removeState($this->_request, 'test');
+        $this->assertNull(SessionHelper::getState($this->_request, 'test'));
+    }
+}
