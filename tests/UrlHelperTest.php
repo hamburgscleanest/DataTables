005f576b31f4285dc9abebedf1665623c486b954
Chroma <chroma91@gmail.com>
2017-04-09 23:00:09 +0200

    UrlHelper: Tests

---
+++ tests/UrlHelperTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace hamburgscleanest\DataTables\Tests;
+
+use hamburgscleanest\DataTables\Helpers\UrlHelper;
+use RuntimeException;
+
+/**
+ * Class UrlHelperTest
+ * @package hamburgscleanest\DataTables\Tests
+ */
+class UrlHelperTest extends TestCase {
+
+
+    /**
+     * @test
+     */
+    public function query_parameters_are_set_correctly()
+    {
+        $this->assertEquals(
+            [
+                'test'  => 'abc',
+                'test2' => 'def'
+            ],
+            UrlHelper::parameterizeQuery('test=abc&test2=def')
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function exception_is_thrown_for_malformed_query()
+    {
+        $this->expectException(RuntimeException::class);
+        UrlHelper::parameterizeQuery('test');
+    }
+}
