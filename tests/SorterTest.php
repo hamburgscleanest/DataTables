469abdec1c5b853be24e1d93bcd4cb2b0d7001eb
Chroma91 <chroma91@gmail.com>
2017-04-11 17:43:38 +0200

    Sorter: Improved code coverage

--- tests/SorterTest.php
+++ tests/SorterTest.php
@@ -3,6 +3,7 @@
 namespace hamburgscleanest\DataTables\Tests;
 
 use hamburgscleanest\DataTables\Facades\DataTable;
+use hamburgscleanest\DataTables\Facades\SessionHelper;
 use hamburgscleanest\DataTables\Models\DataComponents\Sorter;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
@@ -14,6 +15,13 @@ class SorterTest extends TestCase {
 
     use DatabaseMigrations;
 
+    public function setUp()
+    {
+        parent::setUp();
+
+        SessionHelper::shouldReceive('removeState');
+    }
+
     /**
      * @test
      */
@@ -22,7 +30,6 @@ class SorterTest extends TestCase {
         /** @var Sorter $sorter */
         $sorter = new Sorter(['created_at' => 'asc']);
 
-        /** @var \hamburgscleanest\DataTables\Models\DataTable $dataTable */
         $dataTable = DataTable::model(TestModel::class, ['id', 'created_at', 'name'])
             ->addComponent($sorter);
 
@@ -44,7 +51,6 @@ class SorterTest extends TestCase {
         $sorter = new Sorter();
         $sorter->addField('created_at', 'asc');
 
-        /** @var \hamburgscleanest\DataTables\Models\DataTable $dataTable */
         $dataTable = DataTable::model(TestModel::class, ['id', 'created_at', 'name'])
             ->addComponent($sorter);
 
@@ -52,6 +58,60 @@ class SorterTest extends TestCase {
         TestModel::create(['name' => 'test', 'created_at' => $date]);
 
         $this->assertEquals(
+            '<table class="table"><tr><th>id</th><th>created_at</th><th>name</th></tr><tr><td>101</td><td>' . $date . '</td><td>test</td></tr><tr>',
+            \mb_substr($dataTable->render(), 0, 139)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function sorting_defaults_to_asc()
+    {
+        $date = '1991-08-03 00:00:00';
+        TestModel::create(['name' => 'test', 'created_at' => $date]);
+
+        $dataTable = DataTable::model(TestModel::class, ['id', 'created_at', 'name'])->addComponent(new Sorter(['created_at']));
+
+        $this->assertEquals(
+            '<table class="table"><tr><th>id</th><th>created_at</th><th>name</th></tr><tr><td>101</td><td>' . $date . '</td><td>test</td></tr><tr>',
+            \mb_substr($dataTable->render(), 0, 139)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function sorting_parameters_are_considered()
+    {
+        request()->request->add(['sort' => 'created_at']);
+
+        $date = '1991-08-03 00:00:00';
+        TestModel::create(['name' => 'test', 'created_at' => $date]);
+
+        $dataTable = DataTable::model(TestModel::class, ['id', 'created_at', 'name'])->addComponent(new Sorter());
+
+        $this->assertEquals(
+            '<table class="table"><tr><th>id</th><th>created_at</th><th>name</th></tr><tr><td>101</td><td>' . $date . '</td><td>test</td></tr><tr>',
+            \mb_substr($dataTable->render(), 0, 139)
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function none_removes_the_column_from_sorting()
+    {
+        $date = '1991-08-03 00:00:00';
+        TestModel::create(['name' => 'test', 'created_at' => $date]);
+
+        request()->request->add(['sort' => 'created_at~none']);
+
+        $dataTable = DataTable::model(TestModel::class, ['id', 'created_at', 'name'])->addComponent(new Sorter(['created_at']));
+
+        $dataTable->render();
+
+        $this->assertNotEquals(
             '<table class="table"><tr><th>id</th><th>created_at</th><th>name</th></tr><tr><td>101</td><td>' . $date . '</td><td>test</td></tr><tr>',
             \mb_substr($dataTable->render(), 0, 139)
         );
