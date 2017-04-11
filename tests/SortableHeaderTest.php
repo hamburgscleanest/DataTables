44b31eca088d1a8001a9b6d552f9497076acffe5
Chroma91 <chroma91@gmail.com>
2017-04-11 15:51:17 +0200

    SortableHeader: Tests

--- tests/SortableHeaderTest.php
+++ tests/SortableHeaderTest.php
@@ -5,6 +5,7 @@ namespace hamburgscleanest\DataTables\Tests;
 use Carbon\Carbon;
 use hamburgscleanest\DataTables\Facades\DataTable;
 use hamburgscleanest\DataTables\Facades\SessionHelper;
+use hamburgscleanest\DataTables\Models\DataComponents\Sorter;
 use hamburgscleanest\DataTables\Models\HeaderFormatters\SortableHeader;
 
 /**
@@ -92,6 +93,35 @@ class SortableHeaderTest extends TestCase {
         $this->assertEquals(
             '<table class="table"><tr><th>name</th><th><a class="sortable-header" href="' . $this->baseUrl . '?sort=created_at%7Easc">created_at <span class="sort-symbol">⇵</span></a></th></tr><tr><td>' .
             $fieldName . '</td><td>' . $date . '</td></tr></table>',
+            $dataTable->render()
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function sorting_symbols_are_changed()
+    {
+        $fieldName = 'sort-test';
+        TestModel::create(['name' => $fieldName, 'created_at' => Carbon::now()]);
+
+        $symbols = [
+            'asc'  => 'X',
+            'desc' => 'Y',
+            'none' => 'Z'
+        ];
+
+        $sorter = new Sorter();
+
+        $sortableHeader = new SortableHeader(['name']);
+        $sortableHeader->sortingSymbols($symbols);
+
+        $dataTable = DataTable::model(TestModel::class, ['name'])->formatHeaders($sortableHeader)->addComponent($sorter);
+        $dataTable->query()->where('name', $fieldName);
+
+        $this->assertEquals(
+            '<table class="table"><tr><th><a class="sortable-header" href="' . $this->baseUrl .
+            '?sort=name%7Easc">name <span class="sort-symbol">' . $symbols['none'] . '</span></a></th></tr><tr><td>' . $fieldName . '</td></tr></table>',
             $dataTable->render()
         );
     }
