b091bdb3bda327a3127add46150a0e1a2955af53
Chroma <chroma91@gmail.com>
2017-04-13 23:43:30 +0200

    SortableHeader: Tests

--- tests/SortableHeaderTest.php
+++ tests/SortableHeaderTest.php
@@ -186,4 +186,24 @@ class SortableHeaderTest extends TestCase {
             \mb_substr($dataTable->render(), 0, 101)
         );
     }
+
+    /**
+     * @test
+     */
+    public function multiple_existing_parameters_are_considered()
+    {
+        request()->request->add(['sort' => 'created_at~desc.name~asc']);
+
+        $date = '1970-01-01 00:00:00';
+        TestModel::create(['name' => 'sort-request-test', 'created_at' => $date]);
+
+        $dataTable = DataTable::model(TestModel::class, ['created_at', 'name'])->formatHeaders(new SortableHeader(['created_at', 'name']));
+
+        $this->assertEquals(
+            '<table class="table"><tr><th><a class="sortable-header" href="' . $this->baseUrl .
+            '?sort=created_at%7Enone">created_at <span class="sort-symbol">&#x25BC;</span></a></th><th><a class="sortable-header" href="' . $this->baseUrl .
+            '?sort=name%7Edesc',
+            \mb_substr($dataTable->render(), 0, 234)
+        );
+    }
 }
